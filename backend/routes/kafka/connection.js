846542a0cac7a31890561859403dca21ad5ca14d
Ankit Bharadiya <asbharadiya@gmail.com>
2017-10-30 04:46:28 -0700

    auto create topic

--- backend/routes/kafka/connection.js
+++ backend/routes/kafka/connection.js
@@ -7,17 +7,10 @@ function ConnectionProvider() {
             var _this = this;
             var client = new kafka.Client("localhost:2181");
             client.on('ready', function () { 
-                var tId = setInterval(function(){
-                    client.loadMetadataForTopics([topic_name], function (error, results) {
-                        if(Object.keys(results[1].metadata).length > 0){
-                            console.log('consumer ready') 
-                            _this.kafkaConsumerConnection = new kafka.Consumer(client,[{ topic: topic_name, partition: 0 }]);
-                            clearInterval(tId);
-                            callback(_this.kafkaConsumerConnection);
-                        }
-                    });
-                },100);
-                
+                client.loadMetadataForTopics([topic_name], function (error, results) {
+                    _this.kafkaConsumerConnection = new kafka.Consumer(client,[{ topic: topic_name, partition: 0 }]);
+                    callback(_this.kafkaConsumerConnection);
+                });
             })
         } else {
             callback(this.kafkaConsumerConnection);
